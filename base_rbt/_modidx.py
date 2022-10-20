bda7624623bcb85c06a5b9f8a43f374904b4f70c
Hamish Haggerty <hamishhaggerty@Hamishs-MacBook-Air.local>
2022-10-20 14:02:59 +1100

    added some imports and edited settings.ini minimally

--- base_rbt/_modidx.py
+++ base_rbt/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://hamish-haggerty.github.io',
                 'git_url': 'https://github.com/hamish-haggerty/base_rbt',
                 'lib_path': 'base_rbt'},
-  'syms': {'base_rbt.model_base': {'base_rbt.model_base.foo': ('model_base.html#foo', 'base_rbt/model_base.py')}}}
+  'syms': {'base_rbt.model_base': {}}}
